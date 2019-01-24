6caec323f4bc63fdc50daa03912a05f98ae2451b
Titus Johnson <titus.johnson@aftercode.io>
2019-01-24 16:14:26 -0600

    Adds a simple block fill for the home page

---
+++ assets/scripts/blocks.js
@@ -0,0 +1,33 @@
+function gridUpNode(rootNode) {
+  var blockSource = document.createElement("div")
+                    blockSource.classList.add("block")
+  var rowSource = document.createElement("row")
+                  rowSource.classList.add("row")
+
+  var blockWidth = 20
+  var blockHeight = 20
+  var viewportWidth = rootNode.offsetWidth
+  var viewportHeight = rootNode.offsetHeight
+  var columnMax = Math.ceil(viewportWidth / blockWidth)
+  var rowMax = Math.ceil(viewportHeight / blockHeight)
+
+  for(r=0;r<=rowMax;r++) {
+    setTimeout(function() {
+      var newRow = rowSource.cloneNode()
+      rootNode.appendChild(newRow)
+
+      for(c=0;c<=columnMax;c++) {
+        setTimeout(function() {
+          var newBlock = blockSource.cloneNode()
+          newRow.appendChild(newBlock)
+
+        }, c * Math.random() * 120)
+      }
+    }, r * Math.random() * 160)
+  }
+};
+
+var target = document.querySelector("#gridTarget")
+if(target) {
+  gridUpNode(target)
+}
